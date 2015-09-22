7c9d04eaa6fdb31b3b43f9e5a8d1412721b9ecd7
Ubuntu <ubuntu@ip-172-31-40-185.us-west-2.compute.internal>
2015-09-22 01:08:28 +0000

    updated producer script. Tested

--- kafka/bulkLocationProducer.py
+++ kafka/bulkLocationProducer.py
@@ -22,13 +22,22 @@ class Producer():
             crimeLocations = list(reader)
 
         kafka_cluster = self.config['kafka_cluster']
+
+        print "kafka_cluster is:" + kafka_cluster + " done";
         kafka_client = KafkaClient(kafka_cluster)
         kafka_producer = SimpleProducer(kafka_client)
 
+        # To send messages synchronously
+#        kafkaSimple = KafkaClient('52.10.17.219:9092')
+#        producerSimple = SimpleProducer(kafkaSimple, async=True)
+
         count = 0
-        while True:
+        # while True:
+        while (count < 2):
             for loc in crimeLocations:
-                print loc.keys()
+#                print loc.keys()
+                #print "loc is :" + str(loc) + " and loc.keys() is: " + str(loc.keys);
+                #print "kafka.conn is: " + kafka_producer.conn;
                 crimeId = loc["crime_id"]
                 latitude = float(loc['latitude'])
                 longitude = float(loc['longitude'])
@@ -40,8 +49,13 @@ class Producer():
                 }
                 msg['location'] = location
                 kafka_producer.send_messages(self.topic, json.dumps(msg))
+                
+                #producerSimple.send_messages(self.topic, json.dumps(msg))
+                #producerSimple.send_messages('crimeLocation1', 'tajmessage1');
+                
                 print "sending location update for crime %s" % crimeId
             count += 1
+
             print "+++++++++++++FINISH ROUND %d+++++++++++++++++" % count
 
 
@@ -57,7 +71,7 @@ if __name__ == "__main__":
     # fh.setLevel(logging.INFO)
     # logger.addHandler(fh)
     producer = Producer(
-        topic='crimeLocation',
+        topic='crimeLocation1',
         source_file=sys.argv[1]
     )
 
